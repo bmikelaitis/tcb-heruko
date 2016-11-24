5d4820d041aa969454155afd4ac6454282fef0cb
Brandon Mikelaitis <branbun2@gmail.com>
2016-11-24 06:23:31 +0000

    more updates

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
 //= require jquery_ujs
 //= require turbolinks
 //= require jquery.turbolinks
 //= require bootstrap
+//= require jquery
 //= require bootstrap-datepicker
 //= require bootstrap-sprockets
 //= require_tree .
